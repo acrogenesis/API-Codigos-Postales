ad795bbf413eb1990e3db5c80fd641a97e93182c
Adrian Rangel <adrian.rangel@gmail.com>
2015-02-21 02:44:55 -0600

    use stdout for activerecord logger

--- db.rb
+++ db.rb
@@ -1,3 +1,3 @@
 env = ENV['RACK_ENV'] ? ENV['RACK_ENV'] : 'development'
-ActiveRecord::Base.logger = Logger.new('debug.log')
+ActiveRecord::Base.logger = Logger.new(STDOUT)
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || YAML::load(IO.read('config/database.yml'))[env])
