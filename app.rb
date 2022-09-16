acf52793dfed3b0ff0b5c8e38e2dea56f0b19097
Juan Manuel MAffei <juanmamaffei@gmail.com>
2022-09-16 17:50:07 -0300

    feat: add headers for cors avoiding

--- app.rb
+++ app.rb
@@ -9,7 +9,13 @@ Cuba.define do
       env['warden'].authenticate!(:token)
       res.headers['Cache-Control'] = 'max-age=525600, public'
       res.headers['Content-Type'] = 'application/json; charset=utf-8'
+      res.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+      res.headers['Access-Control-Allow-Origin'] = 'http://localhost:3001'
       res.headers['Access-Control-Allow-Origin'] = '*'
+      res.header['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+      res.header['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, Accept, X-User-Email, X-Auth-Token'
+      res.header['Access-Control-Expose-Headers'] = 'X-Total, X-Total-Pages, X-Per-Page, X-Page, X-Next-Page, X-Prev-Page, X-Link'
+
       res.write Oj.dump(PostalCode.where(codigo_postal:)
         .as_json(except: :id), mode: :object)
     end
